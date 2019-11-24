dc2fcc72adb2fb59962dad5651ee5f4cf17a2ca0
vaughn <vaughncato@gmail.com>
2019-11-23 19:50:22 -0500

    Bake in a flag.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #define REPLACE_FILEDB_WITH_DATABASE_INTERFACE 0
 #define CHANGE_PARAMETER_TO_ADAPTER 0
 #define PASS_ADAPTER 0
-#define ADD_LOAD_METHOD_TO_ADAPTER 1
 
 using std::vector;
 using std::string;
@@ -25,12 +24,10 @@ struct Database {
 struct FileDBAdapter : Database {
   FileDB &file_db;
 
-#if ADD_LOAD_METHOD_TO_ADAPTER
   std::vector<std::string> load(const std::string &name) override
   {
     return file_db.load(name);
   }
-#endif
 };
 
 
